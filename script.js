859a669734584b4c687ada7c20c694837587d22d
Nikhil Arya <nikhilarya3108202@gmail.com>
2025-08-28 17:04:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const square = document.querySelectorAll(".square");
+square.forEach((sq)=>{
+	sq.addEventListener("mouseenter",(e)=>{
+		// e.target.style.backgroundColor="red";
+		let length = square.length;
+		for(let i=0;i<length;i++){
+			if(square[i].id!=e.target.id){
+				square[i].style.backgroundColor="#6F4E37"
+			}
+		}
+		
+	})
+	// optional: reset when mouse leaves
+	  sq.addEventListener("mouseleave", (e) => {
+		    let length = square.length;
+		for(let i=0;i<length;i++){
+			if(square[i].id!=e.target.id){
+				square[i].style.backgroundColor="#E6E6FA"
+			}
+		}
+	  });
+})
