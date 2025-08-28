c70b5e28dea8359c92d53092366e7d8ad2b3613c
Nikhil Arya <nikhilarya3108202@gmail.com>
2025-08-28 17:21:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-//your JS code here. If required.
-const square = document.querySelectorAll(".square");
-square.forEach((sq)=>{
-	sq.addEventListener("mouseenter",(e)=>{
-		// e.target.style.backgroundColor="red";
-		let length = square.length;
-		for(let i=0;i<length;i++){
-			if(square[i].id!=e.target.id){
-				square[i].style.backgroundColor="#6F4E37"
-			}
-		}
-		
-	})
-	// optional: reset when mouse leaves
-	  sq.addEventListener("mouseleave", (e) => {
-		    let length = square.length;
-		for(let i=0;i<length;i++){
-			if(square[i].id!=e.target.id){
-				square[i].style.backgroundColor="#E6E6FA"
-			}
-		}
-	  });
-})
+//your JS code here. If required
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((sq) => {
+  sq.addEventListener("mouseenter", (e) => {
+    squares.forEach((el) => {
+      if (el !== e.target) {
+        el.style.backgroundColor = "#6F4E37"; // others
+      }
+    });
+  });
+
+  sq.addEventListener("mouseleave", () => {
+    squares.forEach((el) => {
+      el.style.backgroundColor = "#E6E6FA"; // reset all
+    });
+  });
+});
